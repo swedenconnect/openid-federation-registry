c3fa6948108d142ce908fef719bb26cba889c5fe
”plarsa” <extern.per-fredrik.plars@digg.se>
2025-01-16 19:26:32 +0100

    * Using asymetric keys * Using default settings for resource server * Added key store

--- service/src/main/java/se/swedenconnect/oidf/entity/registry/ExperimentalSecureController.java
+++ service/src/main/java/se/swedenconnect/oidf/entity/registry/ExperimentalSecureController.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 Sweden Connect
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package se.swedenconnect.oidf.entity.registry;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -5,31 +21,56 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This controller provides experimental endpoints for secure operations. It is designed to handle GET and POST requests
+ * and dynamically generates a UUID for each request.
+ *
+ * @author Per Fredrik Plars
+ */
 @RestController
 @RequestMapping("/secure")
 public class ExperimentalSecureController {
-
+  /**
+   * GET
+   *
+   * @return UUID
+   */
   @GetMapping("/policies")
   public String policiesRead() {
     return "Read UUID: " + this.generateUuid();
   }
 
+  /**
+   * Post
+   *
+   * @return UUID
+   */
   @PostMapping("/policies")
   public String policiesWrite() {
     return "Write UUID: " + this.generateUuid();
   }
 
+  /**
+   * GET
+   *
+   * @return UUID
+   */
   @GetMapping
   public String handleGetRequest() {
     return "GET response with UUID: " + this.generateUuid();
   }
 
+  /**
+   * Post
+   *
+   * @return UUID
+   */
   @PostMapping
   public String handlePostRequest() {
     return "POST response with UUID: " + this.generateUuid();
   }
 
-  public String generateUuid() {
+  private String generateUuid() {
     return java.util.UUID.randomUUID().toString();
   }
 }
--- service/src/test/java/se/swedenconnect/oidf/entity/registry/ExperimentalSecureControllerIT.java
+++ service/src/test/java/se/swedenconnect/oidf/entity/registry/ExperimentalSecureControllerIT.java
@@ -1,15 +1,31 @@
 package se.swedenconnect.oidf.entity.registry;
 
-import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.RSASSASigner;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("h2")
 class ExperimentalSecureControllerIT {
@@ -25,9 +41,9 @@ class ExperimentalSecureControllerIT {
           return execution.execute(request, body);
         });
     String url = "/secure";
-    var response = testRestTemplate.getForEntity(url, String.class);
+    final ResponseEntity<String> response = testRestTemplate.getForEntity(url, String.class);
     assertNotNull(response);
-    assertEquals(403, response.getStatusCodeValue());
+    assertEquals(HttpStatusCode.valueOf(403), response.getStatusCode());
   }
 
   @Test
@@ -37,11 +53,11 @@ class ExperimentalSecureControllerIT {
           request.getHeaders().add("Authorization", "Bearer " + createJwt());
           return execution.execute(request, body);
         });
-    String url = "/secure";
-    String requestBody = "sample data";
-    var response = testRestTemplate.postForEntity(url, requestBody, String.class);
+    final String url = "/secure";
+    final String requestBody = "sample data";
+    final ResponseEntity<String> response = testRestTemplate.postForEntity(url, requestBody, String.class);
     assertNotNull(response);
-    assertEquals(403, response.getStatusCodeValue());
+    assertEquals(HttpStatusCode.valueOf(403), response.getStatusCode());
   }
 
   @Test
@@ -53,25 +69,26 @@ class ExperimentalSecureControllerIT {
         });
     String url = "/secure/policies";
     String requestBody = "sample data";
-    var response = testRestTemplate.postForEntity(url, requestBody, String.class);
+    final ResponseEntity<String> response = testRestTemplate.postForEntity(url, requestBody, String.class);
     assertNotNull(response);
-    assertEquals(200, response.getStatusCodeValue());
+    assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
   }
 
   private String createJwt() {
     try {
       final JWTClaimsSet claims = new com.nimbusds.jwt.JWTClaimsSet.Builder()
           .subject("test-user")
+          .audience("https://registry.local.swedenconnect.se")
           .issueTime(new java.util.Date())
-          .expirationTime(new java.util.Date(System.currentTimeMillis() + 1000 * 60 * 10))
+          .expirationTime(Date.from(Instant.now().plus(2, ChronoUnit.DAYS)))
           .issuer("http://swedenconnect.se/op")
           .claim("scope", "entity_read entity_write policies_read policies_write "
               + "trustmarksubject_read trustmarksubject_write")
           .build();
 
-      final MACSigner signer = new com.nimbusds.jose.crypto.MACSigner("00000000000000000000000000000000");
+      final RSASSASigner signer = new RSASSASigner(getPrivateKeyFromKeyStore());
       final SignedJWT signedJwt = new com.nimbusds.jwt.SignedJWT(
-          new com.nimbusds.jose.JWSHeader(com.nimbusds.jose.JWSAlgorithm.HS256),
+          new com.nimbusds.jose.JWSHeader(com.nimbusds.jose.JWSAlgorithm.RS256),
           claims
       );
       signedJwt.sign(signer);
@@ -79,5 +96,14 @@ class ExperimentalSecureControllerIT {
     } catch (Exception e) {
       throw new RuntimeException("Error creating JWT", e);
     }
+  }
+
+  private PrivateKey getPrivateKeyFromKeyStore()
+      throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException, UnrecoverableKeyException {
+    final InputStream keyStoreStream = this.getClass().getResourceAsStream("/keystore.jks");
+    final KeyStore keyStore = KeyStore.getInstance("JKS");
+    keyStore.load(keyStoreStream, "changeit".toCharArray());
+    final Key key = keyStore.getKey("auth", "changeit".toCharArray());
+    return (java.security.PrivateKey) key;
   }
 }
