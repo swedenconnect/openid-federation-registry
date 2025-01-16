aed511ccd0fd946111a4bc789fa282a9e2f12d8a
”plarsa” <extern.per-fredrik.plars@digg.se>
2025-01-16 14:42:05 +0100

    * Optimized imports

--- service/src/main/java/se/swedenconnect/oidf/entity/registry/config/SecurityConfig.java
+++ service/src/main/java/se/swedenconnect/oidf/entity/registry/config/SecurityConfig.java
@@ -6,10 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,7 +22,6 @@ public class SecurityConfig {
 @Bean
 public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
       http
-          // Gör applikationen till en Resource Server
           .oauth2ResourceServer(oauth2 -> oauth2
               .jwt(jwt -> jwt
                   .jwtAuthenticationConverter(new JwtAuthenticationConverter())
